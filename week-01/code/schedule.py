062b09dc619cd4bdad92312e5c816be861aa3f25
Chris Barker <PythonCHB@gmail.com>
2012-06-20 12:46:51 -0700

    tweaked the schedule.py code

--- week-01/code/schedule.py
+++ week-01/code/schedule.py
@@ -4,15 +4,28 @@ Schedule students for lightning talks, fall 2011
 import random
 
 students = open('students.txt').read()
+
 students = students.split('\n')
+
 students.remove('')
+
 random.shuffle(students)
+
 weeks = range(2,11)
-weeks4 = weeks*4
+
+weeks4 = weeks*2
+
 schedule = zip(weeks4, students)
+
 schedule.sort()
+
 outfile = open('schedule.txt', 'w')
+
+print schedule
+
 for week, student in schedule:
-    outfile.write('%s %s\n' % (week, student))
+    line = 'week %s: %s\n' % (week, student)
+    print line, 
+    outfile.write(line)
 outfile.close()
 
