1eca3f5d65aa6ca39a60faa3c6125e02be1b61fa
Chris Barker <PythonCHB@gmail.com>
2012-07-24 14:40:16 -0700

    added the full set of http_serve modules

--- week-05/code/http_serve2.py
+++ week-05/code/http_serve2.py
@@ -2,6 +2,9 @@
 
 import socket 
 
+import httpdate
+
+
 host = '' # listen on all connections (WiFi, etc) 
 port = 50000 
 backlog = 5 # how many connections can we stack up
@@ -20,11 +23,29 @@ s.listen(backlog)
 
 html = open("tiny_html.html").read()
 
+def OK_response(entity):
+    """
+    returns an HTTP response: header and entity in a string
+    """
+    resp = []
+    resp.append('HTTP/1.1 200 OK')
+    resp.append(httpdate.httpdate_now())
+    resp.append('Content-Type: text/html')
+    resp.append('Content-Length: %i'%len(entity))
+    resp.append('')
+    resp.append(entity)
+
+    return "\r\n".join(resp)
+
 while True: # keep looking for new connections forever
     client, address = s.accept() # look for a connection
     request = client.recv(size)
     if request: # if the connection was closed there would be no data
-        print "received:", request
-        client.send(html) 
+        print "received:"
+        print request
+        response = OK_response(html)
+        print "sending:"
+        print response[:120]
+        client.send(response) 
         client.close()
 
