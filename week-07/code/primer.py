6d6d080be83c3cdabaf112877b38869686d4491e
Dan Rutz <DanRutz@hotmail.com>
2012-08-07 02:49:13 -0700

    Some generator examples

---
+++ week-07/code/primer.py
@@ -0,0 +1,50 @@
+import math
+
+def intsum():  # 1 + 2 + 3 + 4 + 5...
+    a = 0
+    while 1:
+        yield (a * (a + 1)) / 2
+        a += 1
+
+def intsum2():  # 1 + 2 + 3 + 4 + 5...
+    a = b = 0
+    while 1:
+        yield b
+        a += 1
+        b = b + a
+
+def dub():  # 1, 2, 4, 8, 16, 32, 64...
+    a = 1
+    while 1:
+        yield a
+        a = a * 2
+
+def fib():  # 1, 1, 2, 3, 5, 8, 13, 21, 34...
+    a, b = 0, 1
+    while 1:
+        yield b
+        a, b = b, a + b
+
+def prime():  # 2, 3, 5, 7, 11, 13, 17, 19, 23...
+    a = 2
+    while 1:
+        yield a
+        p = False
+        while not p:
+            a += 1
+            p = True
+            for x in xrange(2, int(math.floor(math.sqrt(a))) + 1):
+                if a % x == 0:
+                    p = False
+                    break
+
+
+if __name__ == "__main__":
+    v = intsum()
+    w = intsum2()
+    x = dub()
+    y = fib()
+    z = prime()
+    for i in range(101):
+        print i, v.next(), w.next(), x.next(), y.next(), z.next()
+
